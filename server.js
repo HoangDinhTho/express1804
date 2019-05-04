5fde65e786c6beb968df0a27f868daa02a38e343
Huong <huongnguyenak96@gmail.com>
2019-05-04 20:57:23 +0700

    forEach

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     let name = '<i>KPT</i>';
-    res.render('home',{name});
+    let arrPerson = [
+        {name: 'Ti', age: 12},
+        {name: 'Teo', age: 10},
+        {name: 'Nam', age: 11},
+        {name: 'Nu', age: 9}
+    ];
+    
+    res.render('home',{ name, arrPerson });
 })
 app.listen(3000, ()=>console.log('Server start on port 3000'))
