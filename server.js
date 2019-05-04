5dcb6a95209e99e156e9827c0bf6bffef0f8c1bb
Huong <huongnguyenak96@gmail.com>
2019-05-04 20:36:57 +0700

    include

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    let name = 'KPT';
+    let name = '<i>KPT</i>';
     res.render('home',{name});
 })
 app.listen(3000, ()=>console.log('Server start on port 3000'))
